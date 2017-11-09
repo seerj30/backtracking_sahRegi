28a7a596f63a17297cef17ae71b065aa8f46c53e
Sergiu <33357726+seerj30@users.noreply.github.com>
2017-11-09 22:03:20 +0200

    Create sursa.cpp

---
+++ sursa.cpp
@@ -0,0 +1,96 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+int n,nr;
+void init(int st[],int k)
+{
+	st[k]=0;
+}
+
+int succ(int st[],int k)
+{
+	if(st[k]<n)
+	{
+		st[k]++;
+		return 1;
+	}
+	return 0;
+}
+
+
+int valid(int st[],int k)
+{
+	if(k>1 && abs(st[k]-st[k-1])<=1)
+		return 0;
+	return 1;
+}
+
+int sol(int st[],int k)
+{
+	return k==n;
+}
+
+void tipar(int st[],int k)
+{
+	int i,j;
+	char a[25][25];
+	for(i=1;i<=k;i++)
+		for(j=1;j<=k;j++)
+			a[i][j]='*';
+	//completare matrice
+	for(i=1;i<=k;i++)
+		a[i][st[i]]='R';
+	//afisare matrice
+	for(i=1;i<=k;i++)
+	{
+		for(j=1;j<=k;j++)
+			cout<<a[i][j]<<" ";
+		cout<<endl;
+	}
+	//afisare matrice rotita
+	cout<<endl;
+	for(i=1;i<=k;i++)
+	{
+		for(j=1;j<=k;j++)
+			cout<<a[j][i]<<" ";
+		cout<<endl;
+	}
+	cout<<endl;
+	nr+=2;
+}
+
+
+void back(int st[],int k)
+{
+   int as,ev;
+   init(st,k);
+   while(k)
+   {
+		do
+		{
+			as=succ(st,k);
+			if(as)
+				ev=valid(st,k);
+		}while(as && !ev);
+		if(as)
+			if(sol(st,k))
+				tipar(st,k);
+			else
+			{
+				k++;
+				init(st,k);
+			}
+		else
+			k--;
+	}
+}
+
+int main()
+{
+	int st[50];
+	cout<<"n=";
+	cin>>n;
+	back(st,1);
+	cout<<"Numarul de solutii este: "<<nr;
+	return 0;
+}
